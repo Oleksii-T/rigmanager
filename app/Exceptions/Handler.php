7c16d307ef542134de78a4275508d2469e3ffde8
Oleksii Tarbeiev <alex.tarbeev@icloud.com>
2021-03-02 16:28:42 +0200

    Bug fix

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -36,9 +36,6 @@ class Handler extends ExceptionHandler
      */
     public function report(Throwable $exception)
     {
-        if ($this->shouldReport($exception) && app()->bound('sentry')) {
-            app('sentry')->captureException($exception);
-        }
         parent::report($exception);
     }
 
